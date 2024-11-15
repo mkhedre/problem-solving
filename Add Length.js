8d747d8c8c55f1a7a77b9a87ad09b434fc2c4da8
mostafa khedre <moustafa.khedre@rowaad.net>
2024-11-16 00:35:30 +0300

    feat:study accent-color

--- Add Length.js
+++ Add Length.js
@@ -2,16 +2,38 @@
 
 // Add Length
 
-
 // What if we need the length of the words separated
 //  by a space to be added at the end of that same word and have it returned as an array?
-
 
 // Convert the String into Array by Space Sperator
 // Loop through the Array
 // Return the New Word with it's Length
 
 function addLength(str) {
-    return str.split(' ').map((element) => element + ' ' + element.length);
+  return str.split(' ').map((element) => element + ' ' + element.length);
 }
-console.log(addLength('apple ban'));
+
+function binarySearch(arr, low, high, key) {
+  let mid = Math.round((low + high) / 2);
+  if (high < low) return -1;
+  if (key == arr[mid]) return mid;
+  if (key > arr[mid]) {
+    return binarySearch(arr, mid + 1, high, key);
+  }
+  return binarySearch(arr, 0, mid - 1, key);
+}
+let arr = [5, 6, 7, 8, 9, 10];
+let n, key;
+
+n = arr.length;
+key = 10;
+
+console.log(binarySearch(arr, 0, n - 1, key));
+
+function searchInsert(nums, target) {
+  let min = 0;
+  let max = arr.length;
+  let mid = (min + max) / 2;
+}
+// nums = [1,3,5,6], target = 5
+// 2
---
+++ css/notes.md
@@ -0,0 +1,7 @@
+# accent-color: change main user-interface controls like checkbox , radio
+
+https://www.w3schools.com/cssref/index.php .. خلصنا 2
+https://www.w3schools.com/cssref/playdemo.php?filename=playcss_align-content&preval=stretch
+
+will wprk on progress bar which will calculate my progress
+will share good notes
